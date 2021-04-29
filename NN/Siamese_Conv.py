09659a1dfcea5790e2b6ea79a80b1b222eb1a240
Masetto96 <masetto.scola@gmail.com>
2021-04-29 14:18:12 +0200

    updating architecture of siamese network: added fully connected layers

--- NN/Siamese_Conv.py
+++ NN/Siamese_Conv.py
@@ -18,14 +18,40 @@ class SiameseNetwork(nn.Module):
     def __init__(self):
        super(SiameseNetwork, self).__init__()
        self.cnn = nn.Sequential(
-           nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3,3),
+           nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3,3)),
            nn.ReLU(inplace=True),
            nn.MaxPool2d(kernel_size=2),
 
-           nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3),
+           nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3)),
            nn.ReLU(inplace=True),
            nn.MaxPool2d(kernel_size=2),
            nn.Dropout2d(p=0.3),
 
-           nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(3,3)
-           nn.ReLU(inplace=True)
+           nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3)),
+           nn.ReLU(inplace=True),
+           )
+
+       # dense layers
+       self.fc = nn.Sequential(
+
+       nn.Flatten(),
+
+       nn.Linear(in_features = 64*3*3, out_features = 64, bias = True),
+       nn.ReLU(inplace=True),
+
+       nn.Linear(in_features=64, out_features=10, bias=True),
+       nn.Softmax(-1)
+       )
+
+    def forward_once(self, inp):
+        # forward to convolutional layer
+        output = self.cnn(inp)
+        # forward to fully connected layer
+        output = self.fc(output)
+        return output
+
+
+    def forward(self, inp1, inp2):
+        out1 = self.forward_once(inp1)
+        out2 = self.forward_once(inp2)
+        return out1, out2
