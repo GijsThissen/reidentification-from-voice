10eb15f29f3f0442005620171ee638f124220151
MaxBelitsky <md.belitsky@gmail.com>
2021-04-29 19:26:08 +0300

    path fix in train.py + .gitignore update

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 import utils.Data_Loader as DL
 import torchaudio
+import os
 
-data_path = "/home/jakob/data/raw_data.pkl"
+current_dir = os.getcwd()
+data_path = current_dir+"/data/raw_data.pkl"
 
 
 mfcc = torchaudio.transforms.MFCC(sample_rate =  44100, log_mels=True)
@@ -12,7 +14,7 @@ train_data = DL.DataLD(data_path, mfcc)
 
 for i, data in enumerate(train_data.get_loader()):
     print(f"=============\n==============\ntensor: {i}\n============")
-    rec0, rec1, lbl= data
-        #print(f"rec0: {rec0}\n \n rec1: {rec1} \n \n lbl: {lbl}")
+    rec0, rec1, lbl = data
+    #print(f"rec0: {rec0}\n \n rec1: {rec1} \n \n lbl: {lbl}")
     if i > 20:
         break
