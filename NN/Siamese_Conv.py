cbf936b3380d9ed760876c486a5cbea0d38ad865
Masetto96 <masetto.scola@gmail.com>
2021-04-29 23:46:40 +0200

    NN computes distance, tested in train.py

--- NN/Siamese_Conv.py
+++ NN/Siamese_Conv.py
@@ -1,11 +1,13 @@
 import torch
 from torch import nn
+import scipy.spatial
+from torchsummary import summary
 
 
 class ContrastiveLoss(nn.Module):
 
     def __init__(self, margin = 2.0):
-        super.(ContrastiveLoss, self).__init__()
+        super(ContrastiveLoss, self).__init__()
         self.margin = margin
 
     def forward(self, distance, label):
@@ -24,11 +26,11 @@ class SiameseNetwork(nn.Module):
 
            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3)),
            nn.ReLU(inplace=True),
-           nn.MaxPool2d(kernel_size=2),
-           nn.Dropout2d(p=0.3),
+           #nn.MaxPool2d(kernel_size=2),
+           #nn.Dropout2d(p=0.3),
 
-           nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3)),
-           nn.ReLU(inplace=True),
+           #nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3)),
+           #nn.ReLU(inplace=True),
            )
 
        # dense layers
@@ -36,11 +38,11 @@ class SiameseNetwork(nn.Module):
 
        nn.Flatten(),
 
-       nn.Linear(in_features = 64*3*3, out_features = 64, bias = True),
-       nn.ReLU(inplace=True),
+       nn.Linear(in_features = 64*17*25, out_features = 10, bias = True),
+       #nn.ReLU(inplace=True),
 
-       nn.Linear(in_features=64, out_features=10, bias=True),
-       nn.Softmax(-1)
+       #nn.Linear(in_features=64, out_features=10, bias=True),
+       nn.Sigmoid()
        )
 
     def forward_once(self, inp):
@@ -52,6 +54,15 @@ class SiameseNetwork(nn.Module):
 
 
     def forward(self, inp1, inp2):
-        out1 = self.forward_once(inp1)
-        out2 = self.forward_once(inp2)
-        return out1, out2
+        left = self.forward_once(inp1)
+        right = self.forward_once(inp2)
+        # default is euclidian
+        distance = scipy.spatial.distance.cdist(left.detach().numpy(), right.detach().numpy())
+        return distance
+
+
+
+
+
+
+
--- train.py
+++ train.py
@@ -1,20 +1,30 @@
 import utils.Data_Loader as DL
 import torchaudio
 import os
+import NN.Siamese_Conv as NN
 
 current_dir = os.getcwd()
 data_path = current_dir+"/data/raw_data.pkl"
 
-
+# preprocessing function to use when loading the data
 mfcc = torchaudio.transforms.MFCC(sample_rate =  44100, log_mels=True)
 
-
-#cnn
+# loading the data
 train_data = DL.DataLD(data_path, mfcc)
 
-for i, data in enumerate(train_data.get_loader()):
+
+SN = NN.SiameseNetwork()
+
+
+
+for i, data in enumerate(train_data.get_loader(batch_size = 10)):
+
     print(f"=============\n==============\ntensor: {i}\n============")
     rec0, rec1, lbl = data
+    d = SN.forward(rec0, rec1)
+    print("euclidian distance between two vectors:", d)
+    print()
+    print(lbl)
     #print(f"rec0: {rec0}\n \n rec1: {rec1} \n \n lbl: {lbl}")
     if i > 20:
         break
