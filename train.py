f5d29810919ae6383413efa94b934304e8c53e12
Jakob Hauser <jakob.e.hauser@protonmail.com>
2021-05-22 13:17:01 +0200

    training script updates and other stuff

--- train.py
+++ train.py
@@ -7,12 +7,31 @@ import torchaudio
 import os
 import NN.Siamese_Conv as NN
 
+# training_settings
+batch_size = 1000
+n_epochs = 4
+
+# Preprocessing function
+mfcc = torchaudio.transforms.MFCC(sample_rate =  11025, log_mels=True)
+
+# Data
+
+train_data_path = "/home/jakob/src/reidentification-from-voice/data/train_data.pkl"
+val_data_path = "/home/jakob/src/reidentification-from-voice/data/val_data.pkl"
+
+# Loss
+contrastive_loss = NN.ContrastiveLoss()
+
+# Optimizer
+learning_rate = 1e-3
 
 def train(train_data, batch_size,
           preprocessing_function, loss_function,
           learning_rate, n_epochs = 1, deviceid = -1):
 
-    model = NN.SiameseNetwork( conv1_stride = 4, n_embedding_nodes = 100,  )
+    model = NN.SiameseNetwork(  )
+    # conv1_stride = 4, n_embedding_nodes = 100,
+
 
     if deviceid > -1:
         model.cuda()
@@ -30,6 +49,8 @@ def train(train_data, batch_size,
         loss = train_epoch( model, train_data,
                             loss_function, optimzer,
                             batch_size, epoch, deviceid)
+        performance = evalute( model, val_data, deviceid)
+        print(f"dev MAE: {performance}")
         loss_history.append(loss)
 
         #EVAL HERE
@@ -54,7 +75,7 @@ def train_epoch( model, train_data,
     for iter, data in enumerate(data_loader):
 
         rec0, rec1, lbl = data
-        print(len(data_loader))
+        #print(len(data_loader))
 
         if deviceid > -1:
             rec0.requires_grad_().cuda()
@@ -77,24 +98,42 @@ def train_epoch( model, train_data,
 
 
 
-current_dir = os.getcwd()
+def evaluate(model, val_data, deviceid = -1):
 
-# training_settings
-batch_size = 80
-n_epochs = 4
+    model.eval()
+    dataloader = val_data.get_loader(batch_size = 1)
+    num_batches =  len(dataloader)
 
-# Preprocessing function
-mfcc = torchaudio.transforms.MFCC(sample_rate =  11025, log_mels=True)
+    for iter, data in enumerate(data_loader):
 
-# Data
-data_path = "/home/jakob/data/raw_data.pkl"
-train_data = DL.DataLD(data_path, mfcc)
+        rec0, rec1, lbl = data
+        #print(len(data_loader))
 
-# Loss
-contrastive_loss = NN.ContrastiveLoss()
+        if deviceid > -1:
+            rec0.requires_grad_().cuda()
+            rec1.requires_grad_().cuda()
+            lbl.cuda()
+        else:
+            rec0.requires_grad_()
+            rec1.requires_grad_()
+        model.zero_grad()
+        model.batch_size = len(lbl)
+        sim = model(rec0, rec1)
+        error = abs(lbl.detach() - sim.detach())
+        accum_error += error
 
-# Optimizer
-learning_rate = 1e-3
+    return accum_error / num_batches
 
 
-train(train_data, batch_size, mfcc, contrastive_loss, learning_rate, n_epochs)
+
+
+
+def main():
+
+    current_dir = os.getcwd()
+    train_data = DL.DataLD(train_data_path, mfcc)
+    val_data =  DL.DataLD(val_data_path, mfcc)
+
+    train(train_data, batch_size, mfcc, contrastive_loss, learning_rate, n_epochs)
+if __name__  == "__main__":
+    main()
