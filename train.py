e7d68266a85407e6bb29c7dbe51975cb12ef10c7
Jakob Hauser <jakob.e.hauser@protonmail.com>
2021-05-22 13:22:35 +0200

    added stuff

--- train.py
+++ train.py
@@ -6,9 +6,10 @@ import utils.Data_Loader as DL
 import torchaudio
 import os
 import NN.Siamese_Conv as NN
+import time
 
 # training_settings
-batch_size = 1000
+batch_size = 64
 n_epochs = 4
 
 # Preprocessing function
@@ -16,8 +17,8 @@ mfcc = torchaudio.transforms.MFCC(sample_rate =  11025, log_mels=True)
 
 # Data
 
-train_data_path = "/home/jakob/src/reidentification-from-voice/data/train_data.pkl"
-val_data_path = "/home/jakob/src/reidentification-from-voice/data/val_data.pkl"
+train_data_path = "/home/scrappy/src/reidentification-from-voice/data/train_data.pkl"
+val_data_path = "/home/scrappy/src/reidentification-from-voice/data/val_data.pkl"
 
 # Loss
 contrastive_loss = NN.ContrastiveLoss()
@@ -25,7 +26,8 @@ contrastive_loss = NN.ContrastiveLoss()
 # Optimizer
 learning_rate = 1e-3
 
-def train(train_data, batch_size,
+deviceid = 0
+def train(train_data, val_data, batch_size,
           preprocessing_function, loss_function,
           learning_rate, n_epochs = 1, deviceid = -1):
 
@@ -49,7 +51,7 @@ def train(train_data, batch_size,
         loss = train_epoch( model, train_data,
                             loss_function, optimzer,
                             batch_size, epoch, deviceid)
-        performance = evalute( model, val_data, deviceid)
+        performance = evaluate( model, val_data, deviceid)
         print(f"dev MAE: {performance}")
         loss_history.append(loss)
 
@@ -71,12 +73,19 @@ def train_epoch( model, train_data,
 
 
     data_loader = train_data.get_loader(shuf= True, batch_size = batch_size)
+    # print(len(data_loader))
 
+    # starttime = time.time()
     for iter, data in enumerate(data_loader):
-
         rec0, rec1, lbl = data
         #print(len(data_loader))
 
+        if iter == 10:
+            # endtime = time.time()
+            # batch_time =  endtime - starttime
+            # epoch_time = (batch_time/ 10) * 50000 / 60 / 60
+            # print(epoch_time)
+            break
         if deviceid > -1:
             rec0.requires_grad_().cuda()
             rec1.requires_grad_().cuda()
@@ -99,15 +108,21 @@ def train_epoch( model, train_data,
 
 
 def evaluate(model, val_data, deviceid = -1):
+    print(f"Evaluation")
 
     model.eval()
-    dataloader = val_data.get_loader(batch_size = 1)
-    num_batches =  len(dataloader)
+    data_loader = val_data.get_loader(batch_size = 1)
+    num_batches =  len(data_loader)
 
+    accum_error = 0
     for iter, data in enumerate(data_loader):
 
         rec0, rec1, lbl = data
         #print(len(data_loader))
+
+        if iter == 1000:
+            break
+
 
         if deviceid > -1:
             rec0.requires_grad_().cuda()
@@ -117,12 +132,14 @@ def evaluate(model, val_data, deviceid = -1):
             rec0.requires_grad_()
             rec1.requires_grad_()
         model.zero_grad()
+
         model.batch_size = len(lbl)
         sim = model(rec0, rec1)
-        error = abs(lbl.detach() - sim.detach())
+        print(lbl.detach())
+        error = torch.nn.functional.binary_cross_entropy(sim.detach(), lbl.detach())
         accum_error += error
 
-    return accum_error / num_batches
+    return accum_error / 1000
 
 
 
@@ -131,9 +148,9 @@ def evaluate(model, val_data, deviceid = -1):
 def main():
 
     current_dir = os.getcwd()
-    train_data = DL.DataLD(train_data_path, mfcc)
-    val_data =  DL.DataLD(val_data_path, mfcc)
+    train_data = DL.DataLD(train_data_path, mfcc, deviceid)
+    val_data =  DL.DataLD(val_data_path, mfcc, deviceid)
 
-    train(train_data, batch_size, mfcc, contrastive_loss, learning_rate, n_epochs)
+    train(train_data,val_data, batch_size, mfcc, contrastive_loss, learning_rate, n_epochs, deviceid)
 if __name__  == "__main__":
     main()
